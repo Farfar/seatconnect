22d4977c925716686b81ac4f1b35d7d9c3d11850
Farfar <faekie@hotmail.com>
2021-09-27 15:37:38 +0200

    Version bump

--- seatconnect/__version__.py
+++ seatconnect/__version__.py
@@ -1,6 +1,6 @@
 """
-skodaconnect - A Python 3 library for interacting with Skoda Connect and Smartlink services.
+seatconnect - A Python 3 library for interacting with Seat Connect services.
 
-For more details and documentation, visit the github page at https://github.com/lendy007/skodaconnect
+For more details and documentation, visit the github page at https://github.com/farfar/seatconnect
 """
-__version__ = "1.1.2"
+__version__ = "1.1.3"

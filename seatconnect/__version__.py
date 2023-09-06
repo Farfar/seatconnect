ffbf46565282afc8c8a609ef0bda327e47570d66
Farfar <faekie@hotmail.com>
2023-09-06 22:13:00 +0200

    Added updated "garage" endpoint, thanks to @rliegmann. Bumped version to 1.1.8

--- seatconnect/__version__.py
+++ seatconnect/__version__.py
@@ -3,4 +3,4 @@ seatconnect - A Python 3 library for interacting with Seat Connect services.
 
 For more details and documentation, visit the github page at https://github.com/farfar/seatconnect
 """
-__version__ = "1.1.7"
+__version__ = "1.1.8"
